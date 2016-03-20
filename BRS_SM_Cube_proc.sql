e88b853c7ce5691e548cbc8cb3a673879145a62e
trevor-crowley <trevor.crowley@henryschein.ca>
2016-03-19 20:25:17 -0400

    finished Special Markets customer project

--- BRS_SM_Cube_proc.sql
+++ BRS_SM_Cube_proc.sql
@@ -3,7 +3,7 @@ GO
 SET QUOTED_IDENTIFIER ON
 GO
 ALTER PROCEDURE BRS_SM_Cube_proc  
-	@nFiscalMonth as int = 0
+	@nDebug int = 1
 AS
 
 /******************************************************************************
@@ -36,36 +36,39 @@ BEGIN
 	-- interfering with SELECT statements.
 	SET NOCOUNT ON;
 
-Declare @dtSalesDay datetime 
-Declare @nPriorFiscalMonth int, @nFirstFiscalMonth_TY int, @nFiscalMonth int
-
-SET NOCOUNT ON
+Declare @dtSalesDay datetime,  @dtSalesDay_PY datetime
+Declare @nFiscalMonth int, @nFiscalMonth_PY int
+Declare @nFirstFiscalMonth int, @nPriorFiscalMonth int
+Declare @nFirstFiscalMonth_PY int, @nPriorFiscalMonth_PY int 
 
  
 Select
 	@dtSalesDay				= SalesDate,
-	@nFiscalMonth			= CASE WHEN @nFiscalMonth=0 THEN FiscalMonth 			ELSE 0 END,
+	@dtSalesDay_PY			= SalesDate_LY,
 
-	@nPriorFiscalMonth		= CASE WHEN @nFiscalMonth=0 THEN PriorFiscalMonth		ELSE @nFiscalMonth END,
-	@nFirstFiscalMonth_TY	= CASE WHEN @nFiscalMonth=0 THEN FirstFiscalMonth_TY	ELSE @nFiscalMonth END
+	@nFiscalMonth			= FiscalMonth,
+	@nFiscalMonth_PY		= FiscalMonth_LY,
+
+	@nFirstFiscalMonth		= FirstFiscalMonth_TY,
+	@nFirstFiscalMonth_PY	= FirstFiscalMonth_LY,
+
+	@nPriorFiscalMonth		= PriorFiscalMonth,
+	@nPriorFiscalMonth_PY	= (PriorFiscalMonth-100)
 FROM
 	BRS_Rollup_Support02 g
 
 
---
---PRINT '5. MTD CY - Actual from Detail - (CY.MTD.ACT)'
+
+-- '1. MTD CY - Actual from Detail - (CY.MTD.ST.SM)'
 SELECT     
 	t.FiscalMonth, 
 	t.Branch,
 	t.GLBU_Class, 
 	t.SalesDivision, 
 
-	MIN(cg.CustGrp) AS CustomerGroup,
 	t.Shipto,
-	CASE WHEN t.NetSalesAmt = 0 AND dt.FreeGoodsEstInd = 1 and bu.FreeGoodsEstInd = 1 AND mpc.FreeGoodsEstInd = 1 THEN 1 ELSE 0 END AS FreeGoodsEstInd
-
+	CASE WHEN t.NetSalesAmt = 0 AND dt.FreeGoodsEstInd = 1 and bu.FreeGoodsEstInd = 1 AND mpc.FreeGoodsEstInd = 1 THEN 1 ELSE 0 END AS FreeGoodsEstInd,
 	t.OrderSourceCode,
-
 	'A' AS TrxSrc, 
 
 	-- Use Current Seg as MTD is dynamic
@@ -78,7 +81,7 @@ SELECT
 	MIN(glru.GLBU_ClassDS_L1) AS DS_L1,
 	MIN(glru.ReportingClass) AS ReportingClass,
 
-	'CY.MTD.GRP.SM' AS Status,
+	'CY.MTD.ST.SM' AS Status,
 
 
 	SUM(t.NetSalesAmt) AS SalesAmt, 
@@ -126,8 +129,240 @@ GROUP BY
 	,CASE WHEN t.NetSalesAmt = 0 AND dt.FreeGoodsEstInd = 1 and bu.FreeGoodsEstInd = 1 AND mpc.FreeGoodsEstInd = 1 THEN 1 ELSE 0 END
 	,t.OrderSourceCode
 
+UNION ALL
 
---SM Grouped accounts at the ST Level - (SM.ST.ACT)' 
+
+-- '2. MTD CY - Actual from Detail - (CY.MTD.OTHER.SM)'
+SELECT     
+	t.FiscalMonth, 
+	t.Branch,
+	t.GLBU_Class, 
+	t.SalesDivision, 
+
+	0 AS Shipto,
+	CASE WHEN t.NetSalesAmt = 0 AND dt.FreeGoodsEstInd = 1 and bu.FreeGoodsEstInd = 1 AND mpc.FreeGoodsEstInd = 1 THEN 1 ELSE 0 END AS FreeGoodsEstInd,
+	t.OrderSourceCode,
+	'A' AS TrxSrc, 
+
+	-- Use Current Seg as MTD is dynamic
+	c.MarketClass AS MarketClass, 
+	c.SegCd AS SegCd, 
+	c.Specialty AS Specialty, 
+
+	MIN(bu.ReportingClass) AS GLBU_ReportingClass,
+
+	MIN(glru.GLBU_ClassDS_L1) AS DS_L1,
+	MIN(glru.ReportingClass) AS ReportingClass,
+
+	'CY.MTD.OTHER.SM' AS Status,
+
+
+	SUM(t.NetSalesAmt) AS SalesAmt, 
+	CASE WHEN MIN(glru.ReportingClass) = 'NSA' THEN 0 ELSE SUM(NetSalesAmt) - SUM(ExtendedCostAmt) END AS GPAmt, 
+
+	@dtSalesDay AS SalesDate,
+	MIN(t.ID) as UniqueID
+
+FROM         
+	BRS_Transaction AS t
+
+	INNER JOIN dbo.BRS_DocType as dt
+	ON t.DocType = dt.DocType
+ 
+
+	INNER JOIN BRS_BusinessUnitClass as bu
+	ON bu.GLBU_Class = t.GLBU_Class
+
+	INNER JOIN BRS_Customer AS c 
+	ON t.Shipto = c.ShipTo 
+
+	INNER JOIN BRS_CustomerGroup AS cg
+	ON	c.CustGrpWrk = cg.CustGrp
+
+	INNER JOIN BRS_DS_GLBU_Rollup AS glru
+	ON	t.GLBU_Class = glru.GLBU_Class
+
+	INNER JOIN BRS_ItemMPC AS mpc 
+	ON t.MajorProductClass= mpc.MajorProductClass
+
+
+WHERE
+	(t.SalesDate <= @dtSalesDay) AND 
+	(t.FiscalMonth = @nFiscalMonth) AND 
+	(cg.ReportInd = 0 ) AND
+	(1=1)
+
+GROUP BY 
+	t.FiscalMonth
+	,t.Branch
+	,t.GLBU_Class
+	,t.SalesDivision
+
+	,CASE WHEN t.NetSalesAmt = 0 AND dt.FreeGoodsEstInd = 1 and bu.FreeGoodsEstInd = 1 AND mpc.FreeGoodsEstInd = 1 THEN 1 ELSE 0 END
+	,t.OrderSourceCode
+
+	,c.Specialty
+	,c.SegCd
+	,c.MarketClass
+
+UNION ALL
+
+-- '3. MTD PY - Actual from Detail - (PY.MTD.ST.SM)' 
+SELECT     
+	t.FiscalMonth, 
+	t.Branch,
+	t.GLBU_Class, 
+	t.SalesDivision, 
+
+	t.Shipto,
+	CASE WHEN t.NetSalesAmt = 0 AND dt.FreeGoodsEstInd = 1 and bu.FreeGoodsEstInd = 1 AND mpc.FreeGoodsEstInd = 1 THEN 1 ELSE 0 END AS FreeGoodsEstInd,
+	t.OrderSourceCode,
+	'A' AS TrxSrc, 
+
+	-- Use Current Seg as MTD is dynamic
+	MIN(ch.HIST_MarketClass) AS MarketClass, 
+	MIN(ch.HIST_SegCd) AS SegCd, 
+	MIN(ch.HIST_Specialty) AS Specialty, 
+
+	MIN(bu.ReportingClass) AS GLBU_ReportingClass,
+
+	MIN(glru.GLBU_ClassDS_L1) AS DS_L1,
+	MIN(glru.ReportingClass) AS ReportingClass,
+
+	'PY.MTD.ST.SM' AS Status,
+
+
+	SUM(t.NetSalesAmt) AS SalesAmt, 
+	CASE WHEN MIN(glru.ReportingClass) = 'NSA' THEN 0 ELSE SUM(NetSalesAmt) - SUM(ExtendedCostAmt) END AS GPAmt, 
+
+	@dtSalesDay AS SalesDate,
+	MIN(t.ID) as UniqueID
+
+FROM         
+	BRS_Transaction AS t
+
+	INNER JOIN dbo.BRS_DocType as dt
+	ON t.DocType = dt.DocType
+ 
+
+	INNER JOIN BRS_BusinessUnitClass as bu
+	ON bu.GLBU_Class = t.GLBU_Class
+
+	INNER JOIN BRS_Customer AS c 
+	ON t.Shipto = c.ShipTo 
+
+	INNER JOIN BRS_CustomerGroup AS cg
+	ON	c.CustGrpWrk = cg.CustGrp
+
+	INNER JOIN BRS_DS_GLBU_Rollup AS glru
+	ON	t.GLBU_Class = glru.GLBU_Class
+
+	INNER JOIN BRS_ItemMPC AS mpc 
+	ON t.MajorProductClass= mpc.MajorProductClass
+
+	INNER JOIN BRS_CustomerFSC_History AS ch 
+	ON ch.ShipTo = t.Shipto   AND
+		ch.FiscalMonth = t.FiscalMonth
+
+
+WHERE
+	(t.SalesDate <= @dtSalesDay_PY) AND 
+	(t.FiscalMonth = @nFiscalMonth_PY) AND 
+	(cg.ReportInd = 1 ) AND
+	(1=1)
+
+GROUP BY 
+	t.FiscalMonth
+	,t.Branch
+	,t.GLBU_Class
+	,t.SalesDivision
+
+	,t.Shipto
+	,CASE WHEN t.NetSalesAmt = 0 AND dt.FreeGoodsEstInd = 1 and bu.FreeGoodsEstInd = 1 AND mpc.FreeGoodsEstInd = 1 THEN 1 ELSE 0 END
+	,t.OrderSourceCode
+
+UNION ALL
+
+
+-- '4. MTD PY - Actual from Detail - (PY.MTD.OTHER.SM)'
+SELECT     
+	t.FiscalMonth, 
+	t.Branch,
+	t.GLBU_Class, 
+	t.SalesDivision, 
+
+	0 AS Shipto,
+	CASE WHEN t.NetSalesAmt = 0 AND dt.FreeGoodsEstInd = 1 and bu.FreeGoodsEstInd = 1 AND mpc.FreeGoodsEstInd = 1 THEN 1 ELSE 0 END AS FreeGoodsEstInd,
+	t.OrderSourceCode,
+	'A' AS TrxSrc, 
+
+	-- Use Historic Seg as PY MTD
+	ch.HIST_MarketClass AS MarketClass, 
+	ch.HIST_SegCd AS SegCd, 
+	ch.HIST_Specialty AS Specialty, 
+
+	MIN(bu.ReportingClass) AS GLBU_ReportingClass,
+
+	MIN(glru.GLBU_ClassDS_L1) AS DS_L1,
+	MIN(glru.ReportingClass) AS ReportingClass,
+
+	'PY.MTD.OTHER.SM' AS Status,
+
+
+	SUM(t.NetSalesAmt) AS SalesAmt, 
+	CASE WHEN MIN(glru.ReportingClass) = 'NSA' THEN 0 ELSE SUM(NetSalesAmt) - SUM(ExtendedCostAmt) END AS GPAmt, 
+
+	@dtSalesDay AS SalesDate,
+	MIN(t.ID) as UniqueID
+
+FROM         
+	BRS_Transaction AS t
+
+	INNER JOIN dbo.BRS_DocType as dt
+	ON t.DocType = dt.DocType
+ 
+
+	INNER JOIN BRS_BusinessUnitClass as bu
+	ON bu.GLBU_Class = t.GLBU_Class
+
+	INNER JOIN BRS_Customer AS c 
+	ON t.Shipto = c.ShipTo 
+
+	INNER JOIN BRS_CustomerGroup AS cg
+	ON	c.CustGrpWrk = cg.CustGrp
+
+	INNER JOIN BRS_DS_GLBU_Rollup AS glru
+	ON	t.GLBU_Class = glru.GLBU_Class
+
+	INNER JOIN BRS_ItemMPC AS mpc 
+	ON t.MajorProductClass= mpc.MajorProductClass
+
+	INNER JOIN BRS_CustomerFSC_History AS ch 
+	ON ch.ShipTo = t.Shipto   AND
+		ch.FiscalMonth = t.FiscalMonth
+
+
+WHERE
+	(t.SalesDate <= @dtSalesDay_PY) AND 
+	(t.FiscalMonth = @nFiscalMonth_PY) AND 
+	(cg.ReportInd = 0 ) AND
+	(1=1)
+
+GROUP BY 
+	t.FiscalMonth
+	,t.Branch
+	,t.GLBU_Class
+	,t.SalesDivision
+
+	,CASE WHEN t.NetSalesAmt = 0 AND dt.FreeGoodsEstInd = 1 and bu.FreeGoodsEstInd = 1 AND mpc.FreeGoodsEstInd = 1 THEN 1 ELSE 0 END
+	,t.OrderSourceCode
+
+	,ch.HIST_Specialty
+	,ch.HIST_SegCd
+	,ch.HIST_MarketClass
+
+UNION ALL
+--5. SM Grouped accounts at the ST Level - (HIST.ST.ACT)' 
 
 SELECT     
 	t.FiscalMonth, 
@@ -135,11 +370,9 @@ SELECT
 	t.GLBU_Class, 
 	t.SalesDivision, 
 
-	MIN(cg.CustGrp) AS CustomerGroup,
 	t.Shipto,
 	t.FreeGoodsEstInd,
 	t.OrderSourceCode,
-
 	'A' AS TrxSrc, 
 
 	MIN(t.HIST_MarketClass) AS MarketClass, 
@@ -151,10 +384,9 @@ SELECT
 	MIN(glru.GLBU_ClassDS_L1)AS DS_L1,
 	MIN(glru.ReportingClass)AS ReportingClass,
 
-	'CY.YTD.GRP.SM' AS Status,
+	'HIST.ST.SM' AS Status,
 
 	SUM(t.SalesAmt) AS SalesAmt, 
-
 	CASE WHEN MIN(glru.ReportingClass) = 'NSA' THEN 0 ELSE SUM(t.GPAmt) END AS GPAmt, 
 
 	@dtSalesDay AS SalesDate,
@@ -178,8 +410,11 @@ FROM
 
 
 WHERE
-	(t.FiscalMonth between @nFirstFiscalMonth_TY AND @nPriorFiscalMonth) AND
 	(cg.ReportInd = 1 ) AND
+		(
+			(t.FiscalMonth between @nFirstFiscalMonth AND @nPriorFiscalMonth) OR
+			(t.FiscalMonth between @nFirstFiscalMonth_PY AND @nPriorFiscalMonth_PY) 
+		) AND
 	(1=1)
 	
 
@@ -193,10 +428,9 @@ GROUP BY
 	,t.FreeGoodsEstInd
 	,t.OrderSourceCode
 
+
 UNION ALL
-
-
---SM Grouped accounts at the ST Level - (SM.ST.ACT)' 
+--6. SM Grouped accounts at the ST Level - (HIST.OTHER.SM)' 
 
 SELECT     
 	t.FiscalMonth, 
@@ -204,7 +438,6 @@ SELECT
 	t.GLBU_Class, 
 	t.SalesDivision, 
 
-	'OTHER' AS CustomerGroup,
 	0 AS Shipto,
 
 	t.FreeGoodsEstInd,
@@ -221,12 +454,12 @@ SELECT
 	glru.GLBU_ClassDS_L1 AS DS_L1,
 	glru.ReportingClass AS ReportingClass,
 
-	'CY.YTD.OTH.SM' AS Status,
+	'HIST.OTHER.SM' AS Status,
 
 	SUM(t.SalesAmt) AS SalesAmt, 
-
 	CASE WHEN MIN(glru.ReportingClass) = 'NSA' THEN 0 ELSE SUM(t.GPAmt) END AS GPAmt, 
 
+	@dtSalesDay AS SalesDate,
 	MIN(t.ID_MAX) as UniqueID
 
 FROM         
@@ -247,8 +480,11 @@ FROM
 
 
 WHERE
-	(t.FiscalMonth between @nFirstFiscalMonth_TY AND @nPriorFiscalMonth) AND
 	(cg.ReportInd = 0 ) AND
+		(
+			(t.FiscalMonth between @nFirstFiscalMonth AND @nPriorFiscalMonth) OR
+			(t.FiscalMonth between @nFirstFiscalMonth_PY AND @nPriorFiscalMonth_PY) 
+		) AND
 	(1=1)
 	
 
@@ -272,12 +508,13 @@ GROUP BY
 	,glru.ReportingClass
 
 
+
 END
 
 GO
 
 
--- Test Run
--- BRS_SM_Cube_proc  201412
+-- Debug
+-- BRS_SM_Cube_proc 
 
 
