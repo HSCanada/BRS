4406e8a52b72603f414ae339852bb41a1647bb35
trevor-crowley <trevor.crowley@henryschein.ca>
2016-12-13 15:55:15 -0500

    init

---
+++ BRS_FreeGoodsRedeem_Customer.sql
@@ -0,0 +1,74 @@
+﻿
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+ALTER VIEW [dbo].[BRS_FreeGoodsRedeem_Customer]
+AS
+
+/******************************************************************************
+**	File: 
+**	Name: BRS_FreeGoodsRedeem_Customer
+**	Desc: Helper view to summarized Free goods for P&L True-up adjustment
+**
+**              
+**	Return values:  
+**
+**	Called by:   
+**             
+**	Parameters:
+**	Input					Output
+**	----------				-----------
+**
+**	Auth: tmc
+**	Date: 13 Dec 16
+*******************************************************************************
+**	Change History
+*******************************************************************************
+**	Date:	Author:		Description:
+**	-----	----------	--------------------------------------------
+**    
+*******************************************************************************/
+
+
+SELECT     
+	f.ShipTo, 
+	c.SalesCategory_FreeGoodsRedeem_Rollup AS SalesCategory_Rollup, 
+	f.FiscalMonth, 
+	SUM(f.ExtFileCostAmt) AS FreeGoodsRedeem_ExtFileCostAmt
+
+FROM         
+
+	BRS_FreeGoodsRedeem AS f 
+	INNER JOIN BRS_Item AS i 
+	ON f.Item = i.Item 
+
+	INNER JOIN BRS_ItemSalesCategory AS c 
+	ON i.SalesCategory = c.SalesCategory
+
+WHERE     
+	(f.FiscalMonth =
+          (SELECT     PriorFiscalMonth
+            FROM          BRS_Config)
+	)
+GROUP BY 
+	f.FiscalMonth, 
+	f.ShipTo, 
+	c.SalesCategory_FreeGoodsRedeem_Rollup
+
+GO
+
+SET ANSI_NULLS OFF
+GO
+SET QUOTED_IDENTIFIER OFF
+GO
+
+-- Select * from BRS_FreeGoodsRedeem_Customer
+
+
+
+
+
+
+
---
+++ STAGE_BRS_FreeGoodsRedeem_Load.sql
@@ -0,0 +1,63 @@
+﻿
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+
+ALTER VIEW [dbo].[STAGE_BRS_FreeGoodsRedeem_Load]
+AS
+
+/******************************************************************************
+**	File: 
+**	Name: STAGE_BRS_FreeGoodsRedeem_Load
+**	Desc: Helper view to normalize Free goods for loading to production
+**
+**              
+**	Return values:  
+**
+**	Called by:   
+**             
+**	Parameters:
+**	Input					Output
+**	----------				-----------
+**
+**	Auth: tmc
+**	Date: 12 Dec 16
+*******************************************************************************
+**	Change History
+*******************************************************************************
+**	Date:	Author:		Description:
+**	-----	----------	--------------------------------------------
+**    
+*******************************************************************************/
+
+
+SELECT     
+	Item, 
+	SalesOrderNumber, 
+	FiscalMonth, 
+	SUM(ExtFileCostAmt)	AS ExtFileCostAmt, 
+	MIN(ShipTo)			AS ShipTo, 
+	MIN(Supplier)		AS Supplier
+FROM         
+	STAGE_BRS_FreeGoodsRedeem
+GROUP BY 
+	FiscalMonth, 
+	SalesOrderNumber, 
+	Item
+
+GO
+
+SET ANSI_NULLS OFF
+GO
+SET QUOTED_IDENTIFIER OFF
+GO
+
+-- Select * from STAGE_BRS_FreeGoodsRedeem_Load
+
+
+
+
+
+
+
