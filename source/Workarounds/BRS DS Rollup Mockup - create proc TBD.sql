010e3d7d91c4e86a025a5c441312a2c2e4e6fcd9
Crowley <Trevor.Crowley@henryschein.ca>
2018-04-04 15:01:08 -0400

    me maint

--- source/Workarounds/BRS DS Rollup Mockup - create proc TBD.sql
+++ source/Workarounds/BRS DS Rollup Mockup - create proc TBD.sql
@@ -726,7 +726,7 @@ where
 --	6 May 16	tmc		Fixed missing FSC for adjustments
 --	(DocType <> 'AA') And
 	(NOT EXISTS (SELECT * FROM BRS_CustomerFSC_History h WHERE h.Shipto = t.Shipto AND  h.FiscalMonth = t.FiscalMonth)) AND
-	(t.FiscalMonth between 201802 and 201802) 
+	(t.FiscalMonth between 201803 and 201803) 
 
 
 
