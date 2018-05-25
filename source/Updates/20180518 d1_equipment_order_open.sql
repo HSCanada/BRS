ab9b3ec8f22cc04aec251d3f91261ca9f5e36936
Crowley <Trevor.Crowley@henryschein.ca>
2018-05-25 17:32:27 -0400

    pro repair stage setup

--- source/Updates/20180518 d1_equipment_order_open.sql
+++ source/Updates/20180518 d1_equipment_order_open.sql
@@ -38,3 +38,68 @@ CREATE TABLE [Integration].[open_order_opordrpt](
 
 GO
 
+--
+
+-- DROP TABLE [Integration].[open_order_prorepr]
+
+CREATE TABLE [Integration].[open_order_prorepr](
+	[work_order_num] [char](10) NOT NULL,
+	[d1_branch] [char](4) NULL,
+	[rma_code] [char](5) NULL,
+	[order_status] [char](2) NULL,
+	[proj_arr_date] [date] NULL,
+	[proj_comp_date] [date] NULL,
+	[arrival_date] [date] NULL,
+	[shipto] [int] NULL,
+	[practice_name] [varchar](40) NULL,
+	[priv_code] [char](5) NULL,
+	[model_number] [varchar](20) NULL,
+	[est_num] [char](6) NOT NULL,
+	[call_type_code] [char](5) NULL,
+	[problem_code] [char](5) NULL,
+	[problem_descr] [varchar](20) NULL,
+	[cause_code] [char](5) NULL,
+	[cause_descr] [varchar](20) NULL,
+	[d1_user_id] [char](10) NULL,
+
+ CONSTRAINT [open_order_prorepr_pk] PRIMARY KEY NONCLUSTERED 
+(
+	[work_order_num] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [USERDATA]
+) ON [USERDATA]
+
+GO
+
+--
+
+-- DROP TABLE [Integration].[inventory_valuation_whvalrpt]
+
+CREATE TABLE [Integration].[inventory_valuation_whvalrpt](
+	[item] [char](10) NULL,
+	[d1_warehouse] [char](10) NULL,
+	[tag_number] [varchar](20) NULL,
+	[supplier] [char](6) NULL,
+	[model_number] [varchar](20) NULL,
+	[item_description] [varchar](40) NULL,
+	[bin_code] [char](10) NULL,
+	[item_class_code] [char](10) NULL,
+	[available_qty] [int] NULL,
+	[allocation_qty] [int] NULL,
+	[reserved_qty] [int] NULL,
+	[total_qty] [int] NULL,
+	[cost_unit] [char](2) NULL,
+	[tag_or_avg_cost] [money] NULL,
+	[tag_cost_ind] [char](1) NULL,
+	[available_extended_value] [money] NULL,
+	[reserved_extended_value] [money] NULL,
+	[reservation_quantity_list] [varchar](25) NULL,
+	[total_extended_value] [money] NULL,
+	[IdKey] [integer] identity(1,1) NOT NULL
+
+ CONSTRAINT [inventory_valuation_whvalrpt_pk] PRIMARY KEY NONCLUSTERED 
+(
+	[IdKey]  ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [USERDATA]
+) ON [USERDATA]
+
+GO
