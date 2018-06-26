2ba46717a8ab4d692204a9f324db074b47eea560
Crowley <Trevor.Crowley@henryschein.ca>
2018-06-26 13:09:41 -0400

    correct load file now that branch is fixed

--- source/Updates/20180518 d1_equipment_order_open.sql
+++ source/Updates/20180518 d1_equipment_order_open.sql
@@ -758,6 +758,7 @@ WHERE
 1=1
 
 --
+--truncate table nes.inventory_valuation_whvalrpt
 
 INSERT INTO nes.inventory_valuation_whvalrpt
                          (SalesDate, item, branch_code, tag_number, tag_date, available_qty, allocation_qty, reserved_qty, total_qty, tag_or_avg_cost, tag_cost_ind, available_extended_value, 
@@ -766,8 +767,13 @@ SELECT
 BRS_Config.SalesDate,
 item,
 branch_code,
+--SUBSTRING(branch_code,2,LEN(branch_code)-2) as branch_code,
 tag_number,
-CAST (LEFT(tag_number,6) AS DATE) tag_date,
+CASE 
+	WHEN tag_number<>'' 
+	THEN CAST (LEFT(tag_number,6) AS DATE)
+	ELSE NULL
+END	AS tag_date,
 available_qty,
 allocation_qty,
 reserved_qty,
@@ -778,20 +784,19 @@ available_extended_value,
 reserved_extended_value,
 reservation_quantity_list,
 total_extended_value,
-bin_code
+UPPER(bin_code) AS bin_code
 FROM            
 Integration.inventory_valuation_whvalrpt AS inventory_valuation_whvalrpt_1
 CROSS JOIN BRS_Config
 
 --
 -- jen fix branch code first
+INSERT INTO [nes].[branch]
+(branch_code, branch_name)
 SELECT        
-distinct (LTRIM(branch_code)) BR
-
+distinct (i.branch_code), '' descr
 FROM            
-Integration.inventory_valuation_whvalrpt AS inventory_valuation_whvalrpt_1
+Integration.inventory_valuation_whvalrpt AS i
 where  
-LTRIM(BRANCH_CODE) = 'BCVA35'
-
-NOT exists (select * from [nes].[branch] where RTRIM(LTRIM(inventory_valuation_whvalrpt_1.branch_code)) = nes.branch.branch_code)
+NOT exists (select * from [nes].[branch] where i.branch_code = nes.branch.branch_code)
 
