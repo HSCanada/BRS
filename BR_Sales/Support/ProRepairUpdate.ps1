b1eb2ece7e166d40ff407d8b58f3f057f4b301a3
Crowley <Trevor.Crowley@henryschein.ca>
2018-10-09 08:59:33 -0400

    init

---
+++ BR_Sales/Support/ProRepairUpdate.ps1
@@ -0,0 +1,19 @@
+﻿$File_path = 'S:\BR\Projects\service_automation\working\'
+
+$file_in  = 'ProRepair-Template.xlsm'
+$file_out = 'ProRepair-Corporate.xlsm'
+
+
+Remove-Item –path ($File_path + $file_out)
+
+$x1 = New-Object -ComObject "Excel.Application"
+$x1.Visible = $false
+$wb = $x1.workbooks.Open($File_path + $file_in)
+
+$wb.refreshAll()
+$wb.SaveAs($File_path + $file_out)
+
+$wb.Close()
+$x1.Quit()
+
+Remove-Variable wb,x1
