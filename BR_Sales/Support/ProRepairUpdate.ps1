0c98f187b991159783126cb1eb93a1f7ceb09d54
Crowley <Trevor.Crowley@henryschein.ca>
2018-10-30 15:45:23 -0400

    add branch deply code

--- BR_Sales/Support/ProRepairUpdate.ps1
+++ BR_Sales/Support/ProRepairUpdate.ps1
@@ -1,19 +1,52 @@
-﻿# updated 23 Oct 18, tmc
+﻿# updated 30 Oct 18, tmc
 
-$File_in = 'S:\BR\Projects\service_automation\working\ProRepair-20181023.xlsm'
-
-$file_out = 'S:\Pro-Repair\Corporate\ProRepair-All.xlsm'
+$File_in = 'S:\BR\Projects\service_automation\working\ProRepair-20181030.xlsm'
 
 
-Remove-Item –path ($file_out) -ErrorAction Ignore
+#$file_out = 'S:\Pro-Repair\Corporate\ProRepair-All.xlsm'
+
 
 $x1 = New-Object -ComObject "Excel.Application"
 #$x1.Visible = $false
 $x1.Visible = $true
 
+# filter and logging happens on open
 $wb = $x1.workbooks.Open($file_in)
 
+# pull new new data into template excel
 $wb.refreshAll()
+
+# Save individulized version by updating Branch_default on "Working" tab (4)
+$Data= $wb.Worksheets.Item(4)
+
+# AAAAA	All
+$file_out = 'S:\BR\Projects\service_automation\working\ProRepair-All.xlsm'
+Remove-Item –path ($file_out) -ErrorAction Ignore
+$Data.Cells.Item(1,2) = 'AAAAA'
+$wb.SaveAs($file_out)
+
+# MNTRL	Montreal
+$file_out = 'S:\BR\Projects\service_automation\working\ProRepair-Montreal.xlsm'
+Remove-Item –path ($file_out) -ErrorAction Ignore
+$Data.Cells.Item(1,2) = 'MNTRL'
+$wb.SaveAs($file_out)
+
+# QUEBC	Quebec
+$file_out = 'S:\BR\Projects\service_automation\working\ProRepair-Quebec.xlsm'
+Remove-Item –path ($file_out) -ErrorAction Ignore
+$Data.Cells.Item(1,2) = 'QUEBC'
+$wb.SaveAs($file_out)
+
+# TORNT	Toronto
+$file_out = 'S:\BR\Projects\service_automation\working\ProRepair-Toronto.xlsm'
+Remove-Item –path ($file_out) -ErrorAction Ignore
+$Data.Cells.Item(1,2) = 'TORNT'
+$wb.SaveAs($file_out)
+
+# VACVR	Vancouver
+$file_out = 'S:\BR\Projects\service_automation\working\ProRepair-Vancouver.xlsm'
+Remove-Item –path ($file_out) -ErrorAction Ignore
+$Data.Cells.Item(1,2) = 'VACVR'
 $wb.SaveAs($file_out)
 
 $wb.Close()
