9777875d58b68789fb75cc4766c198dc4f7863f3
Crowley <Trevor.Crowley@henryschein.ca>
2018-11-05 17:17:12 -0500

    remove connection on publish

--- BR_Sales/Support/ProRepairUpdate.ps1
+++ BR_Sales/Support/ProRepairUpdate.ps1
@@ -1,7 +1,7 @@
 ﻿# ProRepair:  Daily Refresh & file distribution.  
-# updated 31 Oct 18, tmc
+# updated 05 Nov 18, tmc
 
-$File_in = 'S:\BR\Projects\service_automation\working\ProRepair-20181031.xlsm'
+$File_in = 'S:\BR\Projects\service_automation\working\ProRepair-20181105.xlsm'
 
 
 $x1 = New-Object -ComObject "Excel.Application"
@@ -17,34 +17,42 @@ $wb.refreshAll()
 # Save individulized version by updating Branch_default on "Working" tab (4)
 $Data= $wb.Worksheets.Item(4)
 
+# remove connection for reporting
+$wb.Connections.Item(1).Delete()
+
 # AAAAA	All
 $file_out = 'S:\Pro-Repair\Corporate\update\ProRepair-All.xlsm'
 Remove-Item –path ($file_out) -ErrorAction Ignore
 $Data.Cells.Item(1,2) = 'AAAAA'
+$wb.refreshAll()
 $wb.SaveAs($file_out)
 
 # MNTRL	Montreal
 $file_out = 'S:\Pro-Repair\Montreal\update\ProRepair-Montreal.xlsm'
 Remove-Item –path ($file_out) -ErrorAction Ignore
 $Data.Cells.Item(1,2) = 'MNTRL'
+$wb.refreshAll()
 $wb.SaveAs($file_out)
 
 # QUEBC	Quebec
 $file_out = 'S:\Pro-Repair\Quebec\update\ProRepair-Quebec.xlsm'
 Remove-Item –path ($file_out) -ErrorAction Ignore
 $Data.Cells.Item(1,2) = 'QUEBC'
+$wb.refreshAll()
 $wb.SaveAs($file_out)
 
 # TORNT	Toronto
 $file_out = 'S:\Pro-Repair\Toronto\update\ProRepair-Toronto.xlsm'
 Remove-Item –path ($file_out) -ErrorAction Ignore
 $Data.Cells.Item(1,2) = 'TORNT'
+$wb.refreshAll()
 $wb.SaveAs($file_out)
 
 # VACVR	Vancouver
 $file_out = 'S:\Pro-Repair\Vancouver\update\ProRepair-Vancouver.xlsm'
 Remove-Item –path ($file_out) -ErrorAction Ignore
 $Data.Cells.Item(1,2) = 'VACVR'
+$wb.refreshAll()
 $wb.SaveAs($file_out)
 
 $wb.Close()
