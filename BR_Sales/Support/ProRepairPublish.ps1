c38080d378bd800bcfbd5924e289c9217df90c57
Crowley <Trevor.Crowley@henryschein.ca>
2018-11-05 16:56:20 -0500

    init

---
+++ BR_Sales/Support/ProRepairPublish.ps1
@@ -0,0 +1,24 @@
+﻿# ProRepair:  Publish
+# updated 2 Nov 18, tmc
+
+# AAAAA	All
+$file_out = 'S:\Pro-Repair\Corporate\update\ProRepair-All.xlsm'
+Copy-Item $file_out -destination S:\Pro-Repair\Corporate
+
+
+# MNTRL	Montreal
+$file_out = 'S:\Pro-Repair\Montreal\update\ProRepair-Montreal.xlsm'
+Copy-Item $file_out -destination S:\Pro-Repair\Montreal
+
+# QUEBC	Quebec
+$file_out = 'S:\Pro-Repair\Quebec\update\ProRepair-Quebec.xlsm'
+Copy-Item $file_out -destination S:\Pro-Repair\Quebec
+
+# TORNT	Toronto
+$file_out = 'S:\Pro-Repair\Toronto\update\ProRepair-Toronto.xlsm'
+Copy-Item $file_out -destination S:\Pro-Repair\Toronto
+
+# VACVR	Vancouver
+$file_out = 'S:\Pro-Repair\Vancouver\update\ProRepair-Vancouver.xlsm'
+Copy-Item $file_out -destination S:\Pro-Repair\Vancouver
+
