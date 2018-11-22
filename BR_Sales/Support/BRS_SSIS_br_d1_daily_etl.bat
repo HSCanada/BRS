3d9b39f877df40c7b2191de083b8a1e892dd2489
Crowley <Trevor.Crowley@henryschein.ca>
2018-11-22 09:30:20 -0500

    delete source file after load to avoid double run and catch missed days

--- BR_Sales/Support/BRS_SSIS_br_d1_daily_etl.bat
+++ BR_Sales/Support/BRS_SSIS_br_d1_daily_etl.bat
@@ -1,5 +1,7 @@
 @ECHO OFF
 
+:: updated 22 Nov 18, tmc, delete source file after load to avoid double run and catch missed days
+
 SET DB_DST=DEV_BRSales
 IF %BRS_MODE% EQU PROD SET DB_DST=BRSales
 
@@ -13,3 +15,4 @@ osql -S %BRS_SQLSERVER% -E -r -p -Q"exec msdb.dbo.sp_start_job 'SSIS_br_d1_daily
 
 PAUSE
 
+del "..\Upload\prorepr.csv"
