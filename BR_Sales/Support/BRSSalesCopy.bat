7f4625f2aa232981c19b080796187c96582b1231
Crowley <Trevor.Crowley@henryschein.ca>
2018-11-29 15:45:29 -0500

    clear source file after load to ID stale data

--- BR_Sales/Support/BRSSalesCopy.bat
+++ BR_Sales/Support/BRSSalesCopy.bat
@@ -3,5 +3,6 @@ Xcopy S:\DW_Distribution_Services\BRS\BRS*.TXT "..\Upload" /Y
 :: clear manual load files so that dup data not loaded
 
 del "..\Upload\prorepr.csv"
+del S:\DW_Distribution_Services\BRS\BRS*.TXT 
 
 PAUSE
