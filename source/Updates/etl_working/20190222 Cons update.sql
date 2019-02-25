95c9b92b2007fdd3c9122a0a8eaca553103765d9
Crowley <Trevor.Crowley@henryschein.ca>
2019-02-25 10:12:27 -0500

    init

---
+++ source/Updates/etl_working/20190222 Cons update.sql
@@ -0,0 +1,60 @@
+-- refersh Schein Saver from SQ03
+
+truncate table Redemptions_tbl_Main
+GO
+truncate table Redemptions_tbl_Items
+GO
+
+drop table Redemptions_tbl_Items_stage
+GO
+
+drop table Redemptions_tbl_Main_stage
+GO
+
+-- rename redemptions_tbl_Items_stage
+-- rename redemptions_tbl_Items_stagetbl_Main_stage
+
+INSERT INTO [dbo].[Redemptions_tbl_Main]
+           ([RecID]
+           ,[Buy]
+           ,[Get])
+     VALUES
+           (0, '', '')
+GO
+
+INSERT INTO Redemptions_tbl_Main
+                         (RecID, Div, Buy, Get, VendorName, Redeem, Quarter, Note, EffDate, Expired)
+SELECT        RecID, Div, Buy, Get, VendorName, Redeem, Quarter, Note, EffDate, Expired
+
+FROM            Redemptions_tbl_Main_stage
+
+WHERE        (CAST('2019-02-22' AS DATETIME) BETWEEN EffDate AND Expired)
+
+
+
+-- truncate table Redemptions_tbl_Items
+
+INSERT INTO Redemptions_tbl_Items
+	(RecID, ItemNumber, ItemID)
+SELECT        
+	(MAX(i.RecID)), i.ItemNumber, MAX(i.ItemID) AS ItemID
+FROM            
+	Redemptions_tbl_Items_stage AS i 
+
+	INNER JOIN Redemptions_tbl_Main AS d 
+	ON i.RecID = d.RecID 
+	
+	INNER JOIN BRS_Item AS ibr 
+	ON i.ItemNumber = ibr.Item
+
+GROUP BY 
+	i.ItemNumber
+--
+
+-- must be zero
+SELECT        i.ItemNumber, COUNT(d.RecID) AS deal_code
+FROM            Redemptions_tbl_Main AS d INNER JOIN
+                         Redemptions_tbl_Items AS i ON d.RecID = i.RecID
+GROUP BY i.ItemNumber
+HAVING        (COUNT(d.RecID) > 1) order by 2 desc
+
