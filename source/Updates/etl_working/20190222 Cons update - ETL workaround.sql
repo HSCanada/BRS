65f29cbe0a8401e6498b4bdbbc022d8cd9b1e6b1
Crowley <Trevor.Crowley@henryschein.ca>
2019-02-26 10:10:16 -0500

    add redeem logic

--- source/Updates/etl_working/20190222 Cons update - ETL workaround.sql
+++ source/Updates/etl_working/20190222 Cons update - ETL workaround.sql
@@ -1,5 +1,7 @@
 -- refersh Schein Saver from SQ03
 
+
+
 -- clear prod
 truncate table Redemptions_tbl_Main
 GO
@@ -71,3 +73,51 @@ GROUP BY i.ItemNumber
 HAVING        (COUNT(d.RecID) > 1) order by 2 desc
 
 
+
+---
+
+--
+
+-- move to prod
+-- 25 Feb 19, tmc
+
+-- START - prod move
+--drop table [Redemptions_tbl_Items]
+GO
+
+
+CREATE TABLE [dbo].[Redemptions_tbl_Items](
+	[ItemNumber] [nvarchar](12) NOT NULL,
+	[ItemID] [int] NOT NULL,
+	[RecID] [int] NOT NULL,
+ CONSTRAINT [PK_Redemptions_tbl_Items] PRIMARY KEY CLUSTERED 
+(
+	[ItemNumber] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [USERDATA]
+) ON [USERDATA]
+GO
+
+
+--drop table [dbo].[Redemptions_tbl_Main]
+GO
+
+CREATE TABLE [dbo].[Redemptions_tbl_Main](
+	[RecID] [int] NOT NULL,
+	[Div] [varchar](25) NULL,
+	[Buy] [ntext] NULL,
+	[Get] [ntext] NULL,
+	[VendorName] [nvarchar](50) NULL,
+	[Redeem] [nvarchar](1000) NULL,
+	[Quarter] [nvarchar](255) NULL,
+	[Note] [ntext] NULL,
+	[EffDate] [datetime] NULL,
+	[RedeemDate] [datetime] NULL,
+	[Expired] [datetime] NULL,
+ CONSTRAINT [PK_Redemptions_tbl_Main] PRIMARY KEY CLUSTERED 
+(
+	[RecID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [USERDATA]
+) ON [USERDATA] TEXTIMAGE_ON [USERDATA]
+GO
+
+-- END - Prod move
