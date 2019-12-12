1cb5247a03378f6b7f01f9bbde270dd499942863
Crowley <Trevor.Crowley@henryschein.ca>
2019-12-12 17:47:09 -0500

    add credit logic to model

--- source/Updates/20191120 Return tracking.sql
+++ source/Updates/20191120 Return tracking.sql
@@ -2,73 +2,110 @@
 
 BEGIN TRANSACTION
 GO
-ALTER TABLE dbo.BRS_TransactionDW_Ext ADD
+ALTER TABLE [dbo].[BRS_TransactionDW] ADD
 	CreditMinorReasonCode char(4) NULL,
 	CreditTypeCode char(2) NULL
 GO
-ALTER TABLE dbo.BRS_TransactionDW_Ext SET (LOCK_ESCALATION = TABLE)
-GO
 COMMIT
 
-
---
-
+-- load table to 
 -- drop table [dbo].[zzBRSCreditInfo]
 
-alter TABLE [dbo].[zzBRSCreditInfo] ADD
-	id int identity(1,1) NOT NULL
+-- truncate table [Integration].[BRSCreditInfo]
 
-GO
+INSERT INTO [Integration].[BRSCreditInfo]
+(JDEORNO, ORDOTYCD, LNNO, CRMNRECD, CRTYCD, WJXBFS1)
+SELECT        JDEORNO, ORDOTYCD, LNNO, CRMNRECD, CRTYCD, WJXBFS1
+FROM            zzBRSCreditInfo
 
-CREATE TABLE [dbo].[zzBRSCreditInfo](
-	[JDEORNO] [int] NOT NULL,
-	[ORDOTYCD] [nvarchar](50) NOT NULL,
-	[CRMJRECD] [nvarchar](50) NULL,
-	[CRMNRECD] [nvarchar](50) NULL,
-	[CRTYCD] [nvarchar](50) NULL,
-	[WJXBFS1] [float] NULL,
-	id int identity(1,1) NOT NULL
+
+-- drop table [dbo].[BRS_Creditinfo]
+CREATE TABLE [dbo].[BRS_Creditinfo](
+	[CreditMinorReasonCode] [char](4) NOT NULL,
+	[CreditTypeCode] [char](2) NOT NULL,
+	[CreditMinorReason] [nvarchar](30) NOT NULL,
+	[CreditType] [nvarchar](30) NOT NULL,
+	[credit_key] int NOT NULL Identity(1,1)
+ CONSTRAINT [BRS_Creditinfo_c_pk] PRIMARY KEY CLUSTERED 
+(
+	[CreditMinorReasonCode] ASC,
+	[CreditTypeCode] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [USERDATA]
 ) ON [USERDATA]
 GO
 
+ALTER TABLE [dbo].[BRS_Creditinfo] ADD  CONSTRAINT [DF_BRS_Creditinfo_CreditMinorReason]  DEFAULT ('') FOR [CreditMinorReason]
+GO
+
+ALTER TABLE [dbo].[BRS_Creditinfo] ADD  CONSTRAINT [DF_BRS_Creditinfo_CreditType]  DEFAULT ('') FOR [CreditType]
+GO
+
+BEGIN TRANSACTION
+GO
+CREATE UNIQUE NONCLUSTERED INDEX BRS_Creditinfo_u_idx_01 ON dbo.BRS_Creditinfo
+	(
+	credit_key
+	) WITH( STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON USERDATA
+GO
+ALTER TABLE dbo.BRS_Creditinfo SET (LOCK_ESCALATION = TABLE)
+GO
+COMMIT
+
+-- pop initial
+
+INSERT INTO [dbo].[BRS_Creditinfo]
+           ([CreditMinorReasonCode]
+           ,[CreditTypeCode]
+           ,[CreditMinorReason]
+           ,[CreditType])
+     VALUES
+           (''
+           ,''
+           ,'unassigned'
+           ,'unassigned')
+GO
+
+-- pop values
+
+INSERT INTO [dbo].[BRS_Creditinfo]
+([CreditMinorReasonCode], [CreditTypeCode])
+SELECT DISTINCT CRMNRECD AS CreditMinorReasonCode, CRTYCD AS CreditTypeCode
+FROM            Integration.BRSCreditInfo s
+where not exists(select * from [dbo].[BRS_Creditinfo] d where s.CRMNRECD = d.CreditMinorReasonCode AND s.CRTYCD = d.CreditTypeCode)
 
 
+-- add RI
+BEGIN TRANSACTION
+GO
+ALTER TABLE dbo.BRS_TransactionDW ADD CONSTRAINT
+	FK_BRS_TransactionDW_BRS_Creditinfo FOREIGN KEY
+	(
+	CreditMinorReasonCode,
+	CreditTypeCode
+	) REFERENCES dbo.BRS_Creditinfo
+	(
+	CreditMinorReasonCode,
+	CreditTypeCode
+	) ON UPDATE  NO ACTION 
+	 ON DELETE  NO ACTION 
+	
+GO
+ALTER TABLE dbo.BRS_TransactionDW SET (LOCK_ESCALATION = TABLE)
+GO
+COMMIT
 
-UPDATE       BRS_TransactionDW_Ext
-SET                CreditMinorReasonCode = CRMNRECD, CreditTypeCode = CRTYCD
-FROM            
-BRS_TransactionDW_Ext ss
-INNER JOIN
-(
-	SELECT        s.JDEORNO, s.ORDOTYCD, CRMJRECD, s.CRMNRECD, CRTYCD, WJXBFS1, id
-	FROM            zzBRSCreditInfo s
+-- update trans
 
-	INNER JOIN (
+UPDATE BRS_TransactionDW
+	SET CreditMinorReasonCode = s.CRMNRECD,
+	CreditTypeCode = s.CRTYCD
 
-	SELECT        JDEORNO, ORDOTYCD, [CRMNRECD], MIN(id) AS minid
-	FROM            zzBRSCreditInfo
-	GROUP BY JDEORNO, ORDOTYCD, [CRMNRECD]
-	) as sel
-	ON s.ID = sel.minid
-) ss2
-ON ss.SalesOrderNumber = JDEORNO AND
-ss.DocType = ORDOTYCD
+FROM
+	Integration.BRSCreditInfo AS s 
+
+	INNER JOIN BRS_TransactionDW 
+	ON s.JDEORNO = BRS_TransactionDW.SalesOrderNumber AND 
+		s.ORDOTYCD = BRS_TransactionDW.DocType AND 
+		ROUND(s.LNNO * 1000,0) = BRS_TransactionDW.LineNumber
 
 
-
- INNER JOIN
-                         BRS_TransactionDW_Ext AS BRS_TransactionDW_Ext_1 ON BRS_TransactionDW_Ext.SalesOrderNumber = BRS_TransactionDW_Ext_1.SalesOrderNumber
-
-
-/*
-	SELECT        s.JDEORNO, s.ORDOTYCD, CRMJRECD, s.CRMNRECD, CRTYCD, WJXBFS1, id
-	FROM            zzBRSCreditInfo s
-
-	INNER JOIN (
-
-	SELECT        JDEORNO, ORDOTYCD, [CRMNRECD], MIN(id) AS minid
-	FROM            zzBRSCreditInfo
-	GROUP BY JDEORNO, ORDOTYCD, [CRMNRECD]
-	) as sel
-	ON s.ID = sel.minid
-*/
--- source/Updates/etl_working/20171217 global vendor - ETL details.sql
+++ source/Updates/etl_working/20171217 global vendor - ETL details.sql
@@ -86,7 +86,7 @@ FROM
 	BRS_ItemHistory 
 WHERE
 	Excl_key is null AND
-	FiscalMonth BETWEEN 201910 AND 201910
+	FiscalMonth BETWEEN 201911 AND 201911
 GO
 
 
@@ -98,7 +98,7 @@ SET
 FROM
 	BRS_ItemHistory 
 WHERE
-	FiscalMonth BETWEEN 201910 AND 201910
+	FiscalMonth BETWEEN 201911 AND 201911
 GO
 
 print 'set Exclusives - Excl_key, 1s, 1 OF 3'
@@ -118,7 +118,7 @@ FROM
 	ON r.Excl_Code_TargKey = p.Excl_Code  
 WHERE        
 	(r.StatusCd = 1) AND 
-	FiscalMonth BETWEEN 201910 AND 201910
+	FiscalMonth BETWEEN 201911 AND 201911
 GO
 
 
@@ -137,7 +137,7 @@ WHERE
 	(BRS_ItemHistory.Label = 'P') AND 
 	(mpc.PrivateLabelScopeInd = 1) AND 
 	(BRS_ItemHistory.Excl_key IS NULL) AND
-	FiscalMonth BETWEEN 201910 AND 201910
+	FiscalMonth BETWEEN 201911 AND 201911
 GO
 
 
@@ -150,7 +150,7 @@ FROM
 	BRS_ItemHistory 
 WHERE 
 	Excl_key IS NULL and
-	FiscalMonth BETWEEN 201910 AND 201910
+	FiscalMonth BETWEEN 201911 AND 201911
 GO
 
 -- seq 0 of 2
@@ -161,7 +161,7 @@ FROM
 	BRS_Transaction
 WHERE
 	GpsKey is NOT null AND
-	FiscalMonth BETWEEN 201910 AND 201910
+	FiscalMonth BETWEEN 201911 AND 201911
 GO
 
 print 'clear GpsKey, if needed'
@@ -186,7 +186,7 @@ FROM
 	INNER JOIN hfm.gps_code AS g 
 	ON r.Gps_Code_TargKey = g.GpsCode
 WHERE
-	(BRS_Transaction.FiscalMonth between 201910 and 201910)
+	(BRS_Transaction.FiscalMonth between 201911 and 201911)
 GO
 
 -- 1 min
@@ -218,7 +218,7 @@ WHERE
 --	(BRS_Transaction.FiscalMonth between 201701 and 201801)
 -- live
 	(r.Sequence in (110, 120)) AND 
-	(BRS_Transaction.FiscalMonth between 201910 and 201910)
+	(BRS_Transaction.FiscalMonth between 201911 and 201911)
 GO
 
 -- 30s
@@ -250,7 +250,7 @@ WHERE
 --	(BRS_Transaction.FiscalMonth between 201701 and 201801)
 -- live
 	(r.Sequence in (230, 240)) AND 
-	(BRS_Transaction.FiscalMonth between 201910 and 201910)
+	(BRS_Transaction.FiscalMonth between 201911 and 201911)
 GO
 
 print 'test GpsKey - should be > 0 records'
@@ -259,14 +259,14 @@ FROM
 	BRS_Transaction
 WHERE
 	GpsKey is null AND
-	FiscalMonth BETWEEN 201910 AND 201910
+	FiscalMonth BETWEEN 201911 AND 201911
 GO
 
 
 --
 -- 1. set results to file, CSV format
 -- 2. copy below
--- a_CAN_Oct-19_RAr.CSV
+-- a_CAN_Nov-19_RA.CSV
 
 -- 3. select & run below
--- [hfm].global_cube_proc  201910, 201910
+-- [hfm].global_cube_proc  201911, 201911
