d66dc301763723e36948b296bbd689c2a36d6376
Crowley <Trevor.Crowley@henryschein.ca>
2021-02-19 18:17:53 -0500

    maint

--- source/Workarounds/disc.sql
+++ source/Workarounds/disc.sql
@@ -45,8 +45,8 @@ WHERE
 --	(t.SalesCategory = 'TEETH') AND 
 	(t.FreeGoodsInvoicedInd = 0)  And 
 	(
-		(t.FiscalMonth BETWEEN 201901 and 201911) OR
-		(t.FiscalMonth BETWEEN 202001 and 202011)
+		(t.FiscalMonth BETWEEN 202101 and 202101) OR
+		(t.FiscalMonth BETWEEN 202001 and 202001)
 	) AND
 	-- test with quote exclude
 --	(t.OrderSourceCode IN ('A', 'L')) AND 
