3a2083c401fa01c00ea51f1d72316095c0b28437
Crowley <Trevor.Crowley@henryschein.ca>
2023-05-18 13:00:47 -0400

    improve prorepair ETL as per Jen

--- BR_Sales/Support/BRS_SSIS_br_d1_daily_etl.bat
+++ BR_Sales/Support/BRS_SSIS_br_d1_daily_etl.bat
@@ -8,12 +8,16 @@ IF %BRS_MODE% EQU PROD SET DB_DST=BRSales
 ECHO RUN SSIS_br_d1_daily_etl on %BRS_SQLSERVER%.%DB_DST% 
 
 
-SQLCMD -S %BRS_SQLSERVER% -E -Q "USE %DB_DST%; SELECT Max(sales_date) AS [Last Prorepair Loaded] FROM nes.order_open_prorepr_current"
+SQLCMD -S %BRS_SQLSERVER% -E -Q "USE %DB_DST%; SELECT CONVERT(date,MAX([SalesDate]))  FROM nes.order_open_prorepr"
 
 PAUSE
 
 osql -S %BRS_SQLSERVER% -E -r -p -Q"exec msdb.dbo.sp_start_job 'SSIS_br_d1_daily_etl'"
 
+timeout /t 15
+
+SQLCMD -S %BRS_SQLSERVER% -E -Q "USE %DB_DST%; SELECT CONVERT(date,MAX([SalesDate]))  FROM nes.order_open_prorepr"
 
 PAUSE
+
 
