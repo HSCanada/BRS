e192ac872ae5edf43806652a127ed9f3097ae863
Crowley <Trevor.Crowley@henryschein.ca>
2024-02-02 15:02:42 -0500

    add FQDN for dns issues

--- BR_Sales/Support/BRS_Dimension_Load.bat
+++ BR_Sales/Support/BRS_Dimension_Load.bat
@@ -11,14 +11,13 @@ ECHO CLEAR STAGE CUST, ITEM DIMENSION...
 
 SQLCMD -S %BRS_SQLSERVER% -E -Q "USE %DB_DST%; Exec BRS_BE_Dimension_load_proc @bClearStage=1, @bDebug=0"
 
-
 ECHO LOAD STAGE_BRS_CustomerFull...
 bcp %DB_DST%..STAGE_BRS_CustomerFull in ../Upload/BRSCustomerFull.txt -w -T -S %BRS_SQLSERVER% -e BRSCustomerFull_ERR.txt  -F 2
-PAUSE
+::PAUSE
 
 ECHO LOAD STAGE_BRS_ItemFull...
 bcp %DB_DST%..STAGE_BRS_ItemFull in ../Upload/BRSItemFull.txt -w -T -S %BRS_SQLSERVER% -e BRSItemFull_ERR.txt -F 2 -m 40
-PAUSE
+::PAUSE
 
 ECHO LOAD Prod...
 ::PAUSE
--- BR_Sales/Support/BRS_SSIS_br_d1_weekly_etl.bat
+++ BR_Sales/Support/BRS_SSIS_br_d1_weekly_etl.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 
-:: updated 13 Mar 19, tmc, 
+:: updated 29 Jan 24, tmc, fix osql problem
 
 SET DB_DST=DEV_BRSales
 IF %BRS_MODE% EQU PROD SET DB_DST=BRSales
@@ -10,7 +10,11 @@ PAUSE
 
 :: PAUSE
 
-osql -S %BRS_SQLSERVER% -E -r -p -Q"exec msdb.dbo.sp_start_job 'SSIS_br_d1_weekly_etl'"
+::osql -S %BRS_SQLSERVER% -E -r -p -Q"exec msdb.dbo.sp_start_job 'SSIS_br_d1_weekly_etl'"
+sqlcmd -S %BRS_SQLSERVER% -E -d MSDB -Q"exec msdb.dbo.sp_start_job 'SSIS_br_d1_weekly_etl'"
+
+::osql -S %BRS_SQLSERVER% -E -r -p -Q"exec msdb.dbo.sp_start_job 'SSIS_br_d1_daily_etl'"
+::sqlcmd -S %BRS_SQLSERVER% -E -d MSDB -Q "sp_start_job 'SSIS_br_d1_daily_etl'"
 
 
 PAUSE
