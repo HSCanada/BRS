54b6748fa129cb096c0c22060b42df99c1f72d1e
Crowley <Trevor.Crowley@henryschein.ca>
2025-03-14 13:16:39 -0400

    maint

--- source/Updates/20240610 AR pull-make-job.sql
+++ source/Updates/20240610 AR pull-make-job.sql
@@ -61,7 +61,7 @@ GO
 
 -- select top 10 * from [Integration].[F56CUSA2_CUSINF2A]
 -- select count (*) from [Integration].[F56CUSA2_CUSINF2A]
--- 66 191
+-- 66 269
 
 --
 -- truncate table Integration.F56CUSA2_CUSINF2A
