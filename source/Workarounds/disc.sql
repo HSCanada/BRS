216b599aea44fdc48099a3d3041f090f53549bb6
Crowley <Trevor.Crowley@henryschein.ca>
2025-07-04 15:17:52 -0400

    maint

--- source/Workarounds/disc.sql
+++ source/Workarounds/disc.sql
@@ -60,8 +60,8 @@ FROM
 WHERE         
 	(t.SalesCategory = 'MERCH') AND 
 	(
-		(t.FiscalMonth BETWEEN 202501 and 202502) OR
-		(t.FiscalMonth BETWEEN 202401 and 202402)
+		(t.FiscalMonth BETWEEN 202501 and 202506) OR
+		(t.FiscalMonth BETWEEN 202401 and 202406)
 	) AND
 	(
 		t.FreeGoodsInvoicedInd = 0 OR
