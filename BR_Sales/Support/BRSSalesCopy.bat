cb382e77c66f8e26f4b301d4f2b27a0f40da967c
Crowley <Trevor.Crowley@henryschein.ca>
2025-07-04 15:12:20 -0400

    add EastIC item to copy logic

--- BR_Sales/Support/BRSSalesCopy.bat
+++ BR_Sales/Support/BRSSalesCopy.bat
@@ -9,7 +9,7 @@ Xcopy \\usnymeazfs1.hsiweb.local\DropZone\MicroStrategy\Canada_Subscriptions\DW_
 del "..\Upload\prorepr.csv"
 
 del \\usnymeazfs1.hsiweb.local\DropZone\MicroStrategy\Canada_Subscriptions\DW_Distribution_Services\BRS\BRS*.TXT
-del \\usnymeazfs1.hsiweb.local\DropZone\MicroStrategy\Canada_Subscriptions\DW_Distribution_Services\BRS\BRS*.ZIP
+del \\usnymeazfs1.hsiweb.local\DropZone\MicroStrategy\Canada_Subscriptions\DW_Distribution_Services\BRS\*.ZIP
 
 ::del \\usnymeazfs1\DropZone\MicroStrategy\Canada_Subscriptions\DW_Distribution_Services\BRS\BRS*.TXT
 ::del S:\DW_Distribution_Services\BRS\BRS*.TXT 
--- BR_Sales/Support/BRS_TransactionDW_Load.bat
+++ BR_Sales/Support/BRS_TransactionDW_Load.bat
@@ -4,7 +4,6 @@ SET DB_DST=DEV_BRSales
 IF %BRS_MODE% EQU PROD SET DB_DST=BRSales
 
 ECHO Load BRS_BE_Transaction_DW_load_proc to %BRS_SQLSERVER%.%DB_DST% 
-PAUSE
 
 ECHO CLEAR STAGE_BRS_Promotion...
 SQLCMD -S %BRS_SQLSERVER% -E -Q "USE %DB_DST%; Exec BRS_BE_Transaction_DW_load_proc @bClearStage=1, @bDebug=0"
@@ -19,7 +18,6 @@ ECHO LOAD STAGE_BRS_TransactionDW...
 BCP %DB_DST%..STAGE_BRS_TransactionDW in ../Upload/BRS_TransactionDW.txt -w -T -S %BRS_SQLSERVER% -e BRS_TransactionDW_ERR.txt -F 2 -t"|"
 
 ECHO LOAD Prod...
-PAUSE
 SQLCMD -S %BRS_SQLSERVER% -E -Q "USE %DB_DST%; Exec BRS_BE_Transaction_DW_load_proc @bClearStage=0, @bDebug=0; SELECT SalesDateLastWeekly FROM BRS_Config"
 
 ECHO Update ISR consolidator...
