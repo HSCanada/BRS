949a8da1e92158edfea306ec6def150b351e0ab1
Crowley <Trevor.Crowley@henryschein.ca>
2025-07-18 08:12:56 -0400

    maint

--- source/hfm.global_cube.sql
+++ source/hfm.global_cube.sql
@@ -558,7 +558,9 @@ WHERE
 */
  
 
-SELECT top 10 * FROM [hfm].global_cube AS t WHERE PERIOD =  202410 
+SELECT top 10 * FROM [hfm].global_cube AS t WHERE PERIOD =  202506
+
+SELECT * FROM [hfm].global_cube AS t WHERE PERIOD =  202506 and SUPPLIER = 'SIRCER' and ANALYSIS = '3DPRINTING'
 
 -- SELECT count (*) FROM [hfm].global_cube AS t WHERE PERIOD =  202410 
 -- ORG 313 988
