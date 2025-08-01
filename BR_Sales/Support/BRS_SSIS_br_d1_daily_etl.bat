a0c49f6e423d07a6e2188bdf59d5e31caf96cb3d
Crowley <Trevor.Crowley@henryschein.ca>
2025-08-01 11:24:24 -0400

    maint

--- BR_Sales/Support/BRS_SSIS_br_d1_daily_etl.bat
+++ BR_Sales/Support/BRS_SSIS_br_d1_daily_etl.bat
@@ -10,9 +10,6 @@ ECHO RUN SSIS_br_d1_daily_etl on %BRS_SQLSERVER%.%DB_DST%
 
 SQLCMD -S %BRS_SQLSERVER% -E -Q "USE %DB_DST%; SELECT CONVERT(date,MAX([SalesDate]))  FROM nes.order_open_prorepr"
 
-PAUSE
-
-::osql -S %BRS_SQLSERVER% -E -r -p -Q"exec msdb.dbo.sp_start_job 'SSIS_br_d1_daily_etl'"
 sqlcmd -S %BRS_SQLSERVER% -E -d MSDB -Q "sp_start_job 'SSIS_br_d1_daily_etl'"
 
 timeout /t 15
