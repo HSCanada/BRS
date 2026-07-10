a2baef4666739c3344ab8db9c3ff70d7dae49a43
Crowley <Trevor.Crowley@henryschein.ca>
2026-07-10 17:14:42 -0400

    added feedback messages for genpact team

--- BR_Sales/Support/BRS_SSIS_br_d1_daily_etl.bat
+++ BR_Sales/Support/BRS_SSIS_br_d1_daily_etl.bat
@@ -7,12 +7,17 @@ IF %BRS_MODE% EQU PROD SET DB_DST=BRSales
 
 ECHO RUN SSIS_br_d1_daily_etl on %BRS_SQLSERVER%.%DB_DST% 
 
+ECHO check date BEFORE load
 
 SQLCMD -C -S %BRS_SQLSERVER% -E -Q "USE %DB_DST%; SELECT CONVERT(date,MAX([SalesDate]))  FROM nes.order_open_prorepr"
+
+ECHO load pro-repair
 
 sqlcmd -C -S %BRS_SQLSERVER% -E -d MSDB -Q "sp_start_job 'SSIS_br_d1_daily_etl'"
 
 timeout /t 15
+
+ECHO check date AFTER load (should be different from BEFORE)
 
 SQLCMD -C -S %BRS_SQLSERVER% -E -Q "USE %DB_DST%; SELECT CONVERT(date,MAX([SalesDate]))  FROM nes.order_open_prorepr"
 
